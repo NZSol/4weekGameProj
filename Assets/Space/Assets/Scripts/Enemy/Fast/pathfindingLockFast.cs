3e94ae2cebc6c9c0c0adc4a382465953b08d3ba4
Unknown <robert.a.nickel@gmail.com>
2018-08-21 14:54:49 +1200

    Enemy Pathfinding and movement
    Enemy variations now have movement and pathfinding, tracking the player if in range and returning to spawn site if out of range. sprite flipping included

---
+++ Assets/Space/Assets/Scripts/Enemy/Fast/pathfindingLockFast.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pathfindingLockFast : MonoBehaviour {
+
+    public GameObject pathfinder;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = pathfinder.transform.position;
+
+        if (pathfinder.transform.rotation.y > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (pathfinder.transform.rotation.y < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
---
+++ Assets/Space/Assets/Scripts/Enemy/Slow/slowMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slowMovement : MonoBehaviour
+{
+    GameObject Target;
+    Transform targetPos;
+
+    public GameObject spawnLocation;
+    Vector3 spawnPos;
+    Transform spawnSite;
+
+    float distFromTarget;
+    float range = 10;
+    float movementSpeed = 1;
+    float distFromSpawn;
+
+
+    void Awake()
+    {
+        spawnLocation.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+    }
+
+    void Update()
+    {
+        spawnSite = spawnLocation.transform;
+        distFromSpawn = Vector3.Distance(new Vector3(transform.position.x, 0, 0), new Vector3(spawnLocation.transform.position.x, 0, 0));
+
+
+        Target = GameObject.FindWithTag("player");
+        targetPos = Target.transform;
+        distFromTarget = Vector3.Distance(new Vector3(transform.position.x, 0, 0), new Vector3(targetPos.position.x, 0, 0));
+
+        print(spawnSite.transform.position);
+        if (distFromTarget < range)
+        {
+            print("following");
+            pathfind();
+        }
+        else if (distFromTarget > range)
+        {
+            print("returning");
+            returnToSite();
+        }
+
+    }
+
+    void pathfind()
+    {
+        transform.LookAt(targetPos);
+        transform.position += new Vector3(transform.forward.x * movementSpeed * Time.deltaTime, 0, 0);
+    }
+
+    void returnToSite()
+    {
+        transform.LookAt(spawnSite);
+        if (distFromSpawn > 0.1f)
+        {
+            transform.position += new Vector3(transform.forward.x * movementSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+}
